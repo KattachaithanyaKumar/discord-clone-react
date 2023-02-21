f3dfb1001834a2bc3780fb101a79eb8709a896c8
katta chaithanya kumar <80614118+KattachaithanyaKumar@users.noreply.github.com>
2023-02-22 00:12:15 +0530

    register page structure and css

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "./App.css";
 import Background from "./components/Background/Background";
 
 import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 const App = () => {
   return (
     <div className="app">
       <Background />
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./register.css";
 
 const Register = () => {
-  return <div className="register"></div>;
+  return (
+    <div className="register">
+      <div className="register-container">
+        <div className="reg-left"></div>
+        <div className="reg-right">
+          <form>
+            <h1>Register</h1>
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="user name" />
+            <input type="password" placeholder="password" />
+            <input type="password" placeholder="re-enter password" />
+            <button>Register</button>
+          </form>
+          <pre>
+            Already have an account? <button>Login</button>
+          </pre>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
